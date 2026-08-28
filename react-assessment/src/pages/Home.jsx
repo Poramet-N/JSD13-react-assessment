bf79a3dfed1579cebbd1d6e414d7ebce024c38d6
Poramet Nilchan <porametnilchan@gmail.com>
2026-08-28 17:13:55 +0700

    Update file my-understanding.md

--- react-assessment/src/pages/Home.jsx
+++ react-assessment/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return <div className="flex flex-col items-center min-h-screen bg-gray-200 px-4 pt-12">
-    <h1 className="text-3xl md:text-4xl font-bold mb-5 text-black">Generation Thailand<br />
+    <h1 className="text-3xl md:text-4xl font-bold mb-5 text-black">Generation Thailand<br /> 
       React - Assessment</h1>
   </div>;
 }
